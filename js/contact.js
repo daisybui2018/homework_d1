6e77e7dc570ee42847865bd2a68584a30f917d20
cucbk <cucbk@onemount.com>
2021-05-07 09:09:26 +0700

    update js

--- js/contact.js
+++
@@ -1,7 +0,0 @@
-function validateForm() {
-    var x = document.forms["contactForm"]["firstname"].value;
-    if (x == "") {
-      alert("Trường 'Họ' là bắt buộc");
-      return false;
-    }
-  }
