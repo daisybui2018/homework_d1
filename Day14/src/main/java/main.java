8ffd330c87f18180a4e0c169a9513cba8d1bfd96
cucbk <cucbk@onemount.com>
2021-06-11 19:11:57 +0700

    Day14

---
+++ Day14/src/main/java/main.java
@@ -0,0 +1,11 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        // bài 1 tính chỉ số BMI
+        bmi bmi1= new bmi();
+        bmi1.checkBMI();
+
+    }
+}
