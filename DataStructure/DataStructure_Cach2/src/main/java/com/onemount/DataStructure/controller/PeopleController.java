a2ec932590c804fb08bef52e6c0f15d75a3223a5
cucbk <cucbk@onemount.com>
2021-06-27 11:57:48 +0700

    Data Structure

--- DataStructure/DataStructure_Cach2/src/main/java/com/onemount/DataStructure/controller/PeopleController.java
+++ DataStructure/DataStructure_Cach2/src/main/java/com/onemount/DataStructure/controller/PeopleController.java
@@ -21,22 +21,22 @@ public class PeopleController {
     public ResponseEntity<List<Person>> getAll(){
         return ResponseEntity.ok().body(personRepo.getAll());
     }
-
+    //1.1 Đếm số người theo từng quốc tịch in ra màn hình
     @GetMapping("groupbynationalitythencount")
     public ResponseEntity<Map<String, Long>> groupByNationalityThenCount() {
         return ResponseEntity.ok().body(personRepo.countByNationality());
     }
-
+    //1.2 Sắp xếp theo tên những người trên 25 tuổi rồi in ra màn hình
     @GetMapping("getPersonOver25")
     public ResponseEntity<List<Person>> getPersonOver25() {
         return ResponseEntity.ok().body(personRepo.getPersonOver25());
     }
-
+    //1.3 Tính trung bình tuổi của người theo từng quốc gia
     @GetMapping("averageAgePerNationality")
     public ResponseEntity<Map<String, Double>> averageAgePerNationality() {
         return ResponseEntity.ok().body(personRepo.averageAgePerNationality());
     }
-
+    //1.4 In ra màn hình đánh giá tuổi mỗi người
     @GetMapping("addEvaluation")
     public ResponseEntity<List<Person>> addEvaluation(){
         return ResponseEntity.ok().body(personRepo.addEvaluation());
